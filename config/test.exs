08d859b52f815f6d8e484e3ee6bfe32f05b3e751
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2024-11-28 17:37:15 -0800

    More

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,5 @@
 import Config
 
-# Only in tests, remove the complexity from the password hashing algorithm
-config :argon2_elixir, t_cost: 1, m_cost: 8
-
 # Configure your database
 #
 # The MIX_TEST_PARTITION environment variable can be used
@@ -10,23 +7,23 @@ config :argon2_elixir, t_cost: 1, m_cost: 8
 # Run `mix help test` for more information.
 config :core, Core.Repo,
   username: "postgres",
-  password: if(System.get_env("CI"), do: "postgres"),
+  password: "postgres",
   hostname: "localhost",
   database: "core_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: System.schedulers_online() * 2
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :core, CoreWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "7GnUU++jStQkHY0f1Kj72K3ttoieHe4llePjJMlkQcTzoKomuSnIWU6OjNcJKK8x",
+  secret_key_base: "7JNHhcOzH2TvzYjfYS1zTPr1FlzPLyL2QyiOFDxxUG9dmRSNpTeklmq+1+UGdu5+",
   server: false
 
-# In test we don't send emails.
+# In test we don't send emails
 config :core, Core.Mailer, adapter: Swoosh.Adapters.Test
 
-# Disable swoosh api client as it is only required for production adapters.
+# Disable swoosh api client as it is only required for production adapters
 config :swoosh, :api_client, false
 
 # Print only warnings and errors during test
@@ -34,3 +31,7 @@ config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
--- mix.exs
+++ mix.exs
@@ -9,11 +9,7 @@ defmodule Core.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps(),
-      dialyzer: [
-        plt_add_apps: [:mix],
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-      ]
+      deps: deps()
     ]
   end
 
@@ -23,7 +19,7 @@ defmodule Core.MixProject do
   def application do
     [
       mod: {Core.Application, []},
-      extra_applications: [:logger, :runtime_tools, :os_mon]
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
@@ -36,48 +32,42 @@ defmodule Core.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:argon2_elixir, "~> 4.0"},
-      {:phoenix, "~> 1.7", override: true},
-      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix, "~> 1.7.14"},
+      {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
-      {:postgrex, "~> 0.17.1"},
+      {:postgrex, "0.19.3"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.0"},
-      {:floki, "~> 0.35.2", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.0"},
-      {:esbuild, "~> 0.8.0", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.16.0"},
-      {:telemetry_metrics, "~> 0.6.0"},
+      # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
+      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:floki, "0.36.3", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:swoosh, "~> 1.5"},
+      {:finch, "~> 0.13"},
+      {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26.2"},
+      {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:mimic, "~> 1.7", only: :test},
-      {:cors_plug, "~> 3.0"},
-      {:credo, "~> 1.4", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:ecto_psql_extras, "~> 0.7.0"},
-      {:inflex, "~> 2.1"},
-      {:earmark, "~> 1.4"},
-      {:plug_telemetry_server_timing, "~> 0.3.0"},
-      {:slugy, "~> 4.1"},
-      {:hackney, "~> 1.18"},
-      {:timex, "~> 3.7"},
-      {:estate, "~> 1.0"},
+      {:dns_cluster, "~> 0.1.1"},
+      {:bandit, "~> 1.5"},
+      {:yaml_elixir, "~> 2.9"},
+      {:ecto_function, "~> 1.0"},
+      {:ecto_interface, "~> 4.1"},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:ueberauth, "~> 0.7"},
       {:ueberauth_twitch, "~> 0.2.0"},
-      {:bandit, "~> 1.0"},
-      {:ex_machina, "~> 2.7", only: :test},
-      {:yaml_elixir, "~> 2.9"},
-      {:ecto_function, "~> 1.0"},
-      {:ecto_interface, "~> 1.0"},
-      {:ex_heroicons, "~> 2.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:hackney, "~> 1.18"},
+      {:timex, "~> 3.7"},
+      {:ecto_psql_extras, "~> 0.8.2"},
+      {:inflex, "~> 2.1"},
+      {:earmark, "~> 1.4"},
+      {:plug_telemetry_server_timing, "~> 0.3.0"},
+      {:mimic, "~> 1.7", only: :test},
+      {:cors_plug, "~> 3.0"},
+      {:credo, "~> 1.4", only: :dev, runtime: false},
+      {:argon2_elixir, "~> 4.0"}
     ]
   end
 
@@ -89,41 +79,21 @@ defmodule Core.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.build"],
-      "ecto.setup": ["ecto.create --quiet", "ecto.build"],
-      "ecto.seeds": ["run priv/repo/seeds.exs --quiet"],
-      "ecto.fixtures": ["run priv/repo/fixtures.exs --quiet"],
-      "ecto.build": [
-        "ecto.load --quiet",
-        "ecto.migrate",
-        "ecto.dump",
-        "ecto.seeds",
-        "ecto.fixtures"
-      ],
-      "ecto.reload": ["ecto.drop --quiet", "ecto.create --quiet", "ecto.build"],
-      "ecto.reset": [
-        "ecto.drop --quiet",
-        "ecto.create --quiet",
-        "ecto.migrate",
-        "ecto.dump",
-        "ecto.seeds",
-        "ecto.fixtures"
-      ],
-      test: [
-        "ecto.drop --quiet",
-        "ecto.create --quiet",
-        "ecto.load --quiet",
-        "ecto.seeds",
-        "test"
-      ],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": [
         "tailwind.install --if-missing",
         "esbuild.install --if-missing",
         "cmd --cd assets/ npm install"
       ],
-      "assets.build": ["assets.setup", "tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
-      check: ["compile", "credo", "dialyzer --quiet"]
+      "assets.build": ["tailwind core", "esbuild core"],
+      "assets.deploy": [
+        "tailwind core --minify",
+        "esbuild core --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
